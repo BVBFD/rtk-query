742203198c8d430061e384ee48fcaa5abd0b7f25
BVBFD <lsevina126@gmail.com>
2023-05-01 17:34:55 +0900

    ES-Lint(let): Fixed var as let

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react';
 export default function Home() {
   const [testArr, setTestArr] = useState<Array<number | string>>([1, 2, 3, 4, 5]);
 
-  var varTest = 'varTest';
+  let varTest = 'varTest';
 
   useEffect(() => {
     varTest = 'letTest';
